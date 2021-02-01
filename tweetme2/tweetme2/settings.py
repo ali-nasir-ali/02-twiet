5a5915094055e97360560abb1902344d9abdb33f
anasir514 <anasir514@gmail.com>
2021-02-01 21:27:22 +0000

    Update settings.py

--- tweetme2/tweetme2/settings.py
+++ tweetme2/tweetme2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u$bvtigld2ci3i9o53fvl_%y9(x6lwy+nz*blm1lz%@g)7zufy'
+SECRET_KEY = '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

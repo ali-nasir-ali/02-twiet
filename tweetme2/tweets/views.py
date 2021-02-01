a56a83a99d47a749cca4584cd0c6f17d1f634a51
anasir514 <anasir514@gmail.com>
2021-02-01 21:23:34 +0000

    first commit

---
+++ tweetme2/tweets/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.http import HttpResponse , Http404 , JsonResponse
+ 
+from .models import Tweet
+
+def home_view(request,*args,**kwargs):
+   # return HttpResponse('<h1> hello world </h1>')
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_detail_view(request,tweet_id, *args,**kwargs):
+   """
+   rest api view
+   consume by javascript or swift/java/ios/android
+   return json data
+   """
+   data = {
+         "id": tweet_id,
+   }
+   status = 200
+   try:
+       obj = Tweet.objects.get(id=tweet_id)
+       data['content'] = obj.content
+   except:
+       data['message'] = "Not Found"
+       status = 404
+  
+   return JsonResponse(data, status=status) # json.dumps for json type for content_type=application/json
+
